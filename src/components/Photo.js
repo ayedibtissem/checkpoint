e47915654616e144eae77aa42407e4cf5d87644f
ayedibtissem <116677898+ayedibtissem@users.noreply.github.com>
2022-12-22 17:13:38 +0100

    Delete src/components directory

--- src/components/Photo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ts from "./src/components/Profile/Op.jpg";
-function Photo (){
-    return <div className="p"> 
-        <img src= {ts}   alt="pro"width={200}/>
-         </div>
-}
-export default Photo;
